1669421e811d0f54b65ea443a62e88b216b4e48d
Mac <mac@hhwkmh-sl-slmh-belial.local>
2023-06-05 11:59:00 +0200

    reste a faire le chemin en dynamique

--- tsp.py
+++ tsp.py
@@ -5,10 +5,9 @@ import time
 import matplotlib.pyplot as plt
 import networkx.algorithms.approximation as nx_app
 
-named_tuple = time.localtime() # get struct_time
+named_tuple = time.localtime()  # get struct_time
 time_string = time.strftime("%m/%d/%Y, %H:%M:%S", named_tuple)
 print(time_string)
-
 
 arrondissements = [
     "Ahuntsic-Cartierville",
@@ -32,31 +31,71 @@ arrondissements = [
     "Villeray–Saint-Michel–Parc-Extension",
 ]
 
-def chemin_drone():
-    circuits = []
-    for arrondissement in arrondissements:
-        print("Arrondissement :", arrondissement)
-        data = ", Montreal, CANADA"
-        data = arrondissement + data
-        g = ox.graph_from_place(data, network_type="drive")
-        G = nx.Graph(g)
-        print("--- Eulerization du quartier ", arrondissement, " ---")
-        g_eulerize = nx.eulerize(G)
-        print("--- Fin Eulerization ---")
-        print("---Debut de Eulerian circuit---")
-        res = list(nx.eulerian_circuit(g_eulerize))
-        print("---Fin de Eulerian circuit---")
-        circuits.append(res)
-    return circuits
-
 tsp = nx.approximation.traveling_salesman_problem
+"""
+def tsp_quartier(nom):
+    circuits = []
+    print("Arrondissement:", nom)
+    data = nom + ", Montreal, CANADA"
+    g = ox.graph_from_place(data, network_type="drive")
+    #G = nx.Graph(g)
+    G = g.to_undirected()
+    print("--- Eulerization du quartier", nom, " ---")
+    g_eulerize = nx.eulerize(G)
+    print("--- Fin Eulerization ---")
+
+    sol = tsp(G, cycle=False)
+    ox.plot_graph(ox.graph_from_place(data, network_type="drive"), show=False)
+
+    node_coords = nx.get_node_attributes(G, "x_y")
+    node_coords = {k: node_coords[k] for k in sol if k in node_coords}
+    subgraph = G.subgraph(sol)
+
+    paths = []
+    for i, node in enumerate(sol[:-1]):
+        next_node = sol[i + 1]
+
+        if G.has_edge(node, next_node):
+            path = nx.shortest_path(subgraph, node, next_node, weight="length")
+            paths.append(path)
+
+            if node in node_coords:
+                node_coord = node_coords[node]
+                plt.plot(node_coord[0], node_coord[1], "ro", markersize=5)
+
+    last_node = sol[-1]
+    first_node = sol[0]
+
+    if G.has_edge(last_node, first_node):
+        path = nx.shortest_path(subgraph, last_node, first_node, weight="length")
+        paths.append(path)
+
+    ox.plot_graph_route(ox.graph_from_place(data, network_type="drive"), paths, node_size=0, edge_linewidth=2,
+                        edge_color="blue", route_linewidth=4, show=False)
+    
+
+    if first_node in node_coords:
+        node_coord = node_coords[first_node]
+        plt.plot(node_coord[0], node_coord[1], "ro", markersize=5)
+
+    plt.draw()
+    plt.pause(0.5)
+    plt.show()
+
+    return sol
+
+quartier = tsp_quartier("Outremont")
+
+
+"""
 def tsp_quartier(nom):
     circuits = []
     print("Arrondissement :", nom)
     data = ", Montreal, CANADA"
     data = nom + data
     g = ox.graph_from_place(data, network_type="drive")
-    G = nx.Graph(g)
+    #G = nx.Graph(g)
+    G = g.to_undirected()
     print("--- Eulerization du quartier ", nom, " ---")
     g_eulerize = nx.eulerize(G)
     print("--- Fin Eulerization ---")
@@ -66,26 +105,22 @@ def tsp_quartier(nom):
     print(sol)
     #nx.draw(G)
     #plt.show()
-    routes = []
     #for i in range(len(sol) - 1):
      # routes.append(nx.shortest_path(G, sol[i], sol[i + 1]))
-    
     #fig , ax = ox.plot.plot_graph_route(G, sol, save=True, show=False, close=True )
+    #fig , ax = ox.plot.plot_graph_route(G, sol, route_color='r', route_linewidth=4) 
+    fig , ax = ox.plot.plot_graph(G,show=False,close=False, edge_color='r') 
 
-    ###### faut arriver a print le chemin avec les nodes dans sol
-    fig , ax = ox.plot.plot_graph_route(G, sol, route_color='r', route_linewidth=4) 
     #draw_networkx(sol)
     plt.show()
     return sol
 
-#circuits = chemin_drone()
+
+
 quartier = tsp_quartier("Outremont")
 
-
-
-print("--- List des circuits ---")
+#print("--- List des circuits ---")
 #print(quartier)
-
 
 #cycle = quartier
 #edge_list = list(nx.utils.pairwise(cycle))
